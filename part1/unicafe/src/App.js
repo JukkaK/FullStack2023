6ce7fcde37b3d222421b60e338c0545841d9057f
JukkaK <jukka.koskelin@zure.com>
2023-08-13 11:23:55 +0300

    1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.name}</button>
+  )
+}
+
 const Statistics = (props) => {
 
 if(props.total == 0){
@@ -12,46 +18,36 @@ if(props.total == 0){
 
 return(
   <div>
-      <p><b>average</b> {props.average}</p>
-      <p><b>postitive</b> {props.positive} %</p>
+      <StatisticLine text="average" value={props.average} />
+      <StatisticLine text="positive" value={props.positive} />
   </div>
 )
 
+}
+
+const StatisticLine = (props) => {
+  return (
+    <div>
+      {props.text} {props.value}
+    </div>
+  )
 }
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
+  const [bad, setBad] = useState(0)  
+  const total = good + bad + neutral
   const average = (good*1 + neutral*0 + bad*-1)/total
   const positive = good*100/total
 
   return (
     <div>
-      <h1>give feedback</h1>      
-      <button onClick={() => {
-        setGood(good + 1);
-        setTotal(total + 1);
-        console.log('total', total)
-      } } 
-      > good
-      </button>    
-      <button onClick={() => {
-        setNeutral(neutral + 1);
-        setTotal(total + 1);
-        console.log('total', total)
-      } } 
-      > neutral
-      </button>     
-      <button onClick={() => {
-        setBad(bad + 1);
-        setTotal(total + 1);
-        console.log('total', total)
-      } } 
-      > bad
-      </button>   
+      <h1>give feedback</h1>     
+      <Button handleClick={() => setGood(good + 1)} name="Good"/>
+      <Button handleClick={() => setBad(bad + 1)} name="Bad"/>
+      <Button handleClick={() => setNeutral(neutral + 1)} name="Neutral"/>
       <h1>statistics</h1>
       <p><b>good</b> {good}</p>
       <p><b>neutral</b> {neutral}</p>
