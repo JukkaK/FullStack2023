1be69927920bb215a08427510282298f5b787987
JukkaK <jukka.koskelin@zure.com>
2024-02-04 19:23:55 +0200

    something

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,12 +4,14 @@ import Filter from './components/Filter'
 import PersonsForm from './components/PersonsForm'
 import axios from 'axios'
 import personService from './services/persons'
+import './index.css'
 
 const App = () => {
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('');
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     console.log('effect')
@@ -66,6 +68,18 @@ const App = () => {
 
   const namesToShow = persons.filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()));
 
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null
+    }
+  
+    return (
+      <div className='error'>
+        {message}
+      </div>
+    )
+  }
+
   // const Persons = ({ person, deletePerson }) => {
   //   return (
   //     <li>
@@ -78,6 +92,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={errorMessage} />
       <Filter value={nameFilter} onChange={handleNameFilterChange} />
       <div>debug: {nameFilter}</div>
       <PersonsForm 
